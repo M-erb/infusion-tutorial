5c3292325cb08adff5db1d464506f95266d4c395
Michael Erb <Michael Erb>
2016-11-08 18:44:39 -0700

    changed over to ui-route

--- app/angular.config.js
+++ app/angular.config.js
@@ -1,3 +1,43 @@
+tutorialApp.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$localStorageProvider', function($stateProvider, $urlRouterProvider, $locationProvider, $localStorageProvider){
+
+  $locationProvider.html5Mode(true);
+  $localStorageProvider.setKeyPrefix('');
+
+  $stateProvider
+    .state('directory', {
+      url: '/',
+      templateUrl: 'views/directory.html',
+      controller: 'directoryCtrl'
+    });
+  $stateProvider
+    .state('phase1', {
+      url: '/get-started',
+      templateUrl: 'views/get-started.html',
+      controller: 'phase1'
+    });
+
+  $stateProvider
+    .state('phase2', {
+      url: '/users',
+      templateUrl: 'views/users.html',
+      controller: 'phase2'
+    });
+
+
+  $urlRouterProvider.otherwise('directory');
+}])
+
+
+
+
+
+
+
+
+
+
+
+/* -ngRoute-
 tutorialApp.config(['$routeProvider','$locationProvider', '$localStorageProvider', function($routeProvider, $locationProvider, $localStorageProvider){
 
   $locationProvider.html5Mode(true);
@@ -24,3 +64,4 @@ tutorialApp.config(['$routeProvider','$locationProvider', '$localStorageProvider
     });
 
 }]);
+*/
--- app/app.js
+++ app/app.js
@@ -1,60 +1,92 @@
-var tutorialApp = angular.module('tutorialApp', ['ngAnimate','ngRoute','ngclipboard','headroom','checklist-model','ngCookies','ngStorage']);
+var tutorialApp = angular.module('tutorialApp', ['ngAnimate','ui.router','ngclipboard','headroom','checklist-model','ngCookies','ngStorage']);
 
 tutorialApp.controller('directoryCtrl', ['$scope', '$http', '$cookies', '$localStorage', '$sessionStorage', function($scope, $http, $cookies, $localStorage, $sessionStorage){
 
   $scope.storage = $localStorage
   $scope.pageClass = 'directory';
-  /*
-    $http.get('data/directory.json').success(function(Data){
-      $scope.directory = Data;
-    })
-  */
   //house keeping for local storage
   var directoryCheck = function() {
     if ($localStorage.directory == null) {
       $localStorage.directory = {}
     }
-    if ($localStorage.getStarted == null) {
-      $localStorage.getStarted = {}
+    if ($localStorage.phase1 == null) {
+      $localStorage.phase1 = {}
+    }
+    if ($localStorage.phase2 == null) {
+      $localStorage.phase2 = {}
+    }
+    if ($localStorage.phase3 == null) {
+      $localStorage.phase3 = {}
+    }
+    if ($localStorage.phase4 == null) {
+      $localStorage.phase4 = {}
+    }
+    if ($localStorage.phase5 == null) {
+      $localStorage.phase5 = {}
+    }
+    if ($localStorage.phase6 == null) {
+      $localStorage.phase6 = {}
+    }
+    if ($localStorage.phase7 == null) {
+      $localStorage.phase7 = {}
     }
   }
   directoryCheck()
 
   $scope.directory = [{
-    id: "item1",
+    id: "phase1",
     title: "getting started",
     icon: "gettingstarted.png",
     view: "get-started",
     check: true
   }, {
-    id: "item2",
+    id: "phase2",
     title: "users",
     icon: "crm-icon.png",
     view: "users",
-    check: $localStorage.directory.gettingStarted=='done'
+    check: $localStorage.directory.phase1=='done'
+  }, {
+    id: "phase3",
+    title: "CRM - Contacts & Companies",
+    icon: "contact.svg",
+    view: "users",
+    check: $localStorage.directory.phase2=='done'
+  }, {
+    id: "phase4",
+    title: "Branding",
+    icon: "contact.svg",
+    view: "branding",
+    check: $localStorage.directory.phase3=='done'
+  }, {
+    id: "phase5",
+    title: "Sales Pipeline",
+    icon: "contact.svg",
+    view: "sales-pipeline",
+    check: $localStorage.directory.phase4=='done'
+  }, {
+    id: "phase6",
+    title: "Marketing",
+    icon: "contact.svg",
+    view: "marketing",
+    check: $localStorage.directory.phase5=='done'
   }]
 
 }]);
 
 tutorialApp.controller('phase1', ['$scope', '$http', '$cookies', '$localStorage', '$sessionStorage', function($scope, $http, $cookies, $localStorage, $sessionStorage){
   $scope.storage = $localStorage
-
-  $scope.pageClass = 'getStarted';
-
-  $scope.getStartedDone = function() {
-    $localStorage.getingStarted = 'done';
-  };
+  $scope.pageClass = 'getStarted'
 
   $scope.getStartedList = [{
     id: "item1",
     title: "Familiarizing the Layout",
     view: "get-started",
-    check: $localStorage.getStarted.layout=='done'
+    check: $localStorage.phase1.layout=='done'
   }, {
     id: "item2",
     title: "Importing Contacts",
     view: "users",
-    check: $localStorage.getStarted.import=='done'
+    check: $localStorage.phase1.import=='done'
   }]
 
 }]);
